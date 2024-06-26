0c5e560074ff04712f83f71ec62117338dc1875c
wcassie222 <cassandra.washington@jpschools.org>
2024-06-26 16:24:59 +0000

    refactor too create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -32,13 +32,9 @@ view.addChild(circle);
 circles.push(circle);
 }
         // TODO 3 / 7 : Call the drawCircle() function 
-
-       drawCircle();
-       drawCircle();
-       drawCircle();
-       drawCircle();
-       drawCircle();
-
+       for (var i = 0; i < 100; i++) 
+       {drawCircle();}
+       
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -53,19 +49,15 @@ circles.push(circle);
             // TODO 4 : Update the circle's position //
 
             physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);  
-            physikz.updatePosition(circles[4]);
+            
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
             game.checkCirclePosition(circles[0]);
-            game.checkCirclePosition(circles[1]);
-            game.checkCirclePosition(circles[2]);
-            game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);
-
+            
             // TODO 9 : Iterate over the array
-           
+            for (var i = 0; i < 100; i++){
+                game.checkCirclePosition(circles[i]);
+                physikz.updatePosition(circles[i]);
+            }   
             
         }
     
@@ -82,7 +74,16 @@ circles.push(circle);
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+           if (circle.x < 0) {
+            circle.x = canvas.width
+           }
+          if(circle.y > canvas.height) {
+            circle.y = 0 
+          }
+          
+          if (circle.y < 0) {
+            circle.y = canvas.height
+          }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
